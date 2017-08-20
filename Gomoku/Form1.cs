5842ab53642035af0b9ff87f57c3598d9acb062c
chenomg <chenomg@users.noreply.github.com>
2017-08-20 11:02:23 +0800

    2017-8-20 11:02:19

--- Gomoku/Form1.cs
+++ Gomoku/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Gomoku
 {
-    public partial class Form1 : Form
+    public partial class GameForm1 : Form
     {
-        public Form1()
+        public GameForm1()
         {
             InitializeComponent();
         }
